6935397b80260d70c5323b26a2a2d4ae30ceb854
pasha <printcodestudio@gmail.com>
2021-03-04 12:26:55 +0300

    project EH

---
+++ storage/framework/views/2441dc39183fee7600b0b6090317e21a6c16c936.php
@@ -0,0 +1,2 @@
+<p>Hello</p>
+<?php /**PATH W:\domains\EH\resources\views/mail/view.blade.php ENDPATH**/ ?>
--- storage/framework/views/c53088a374615e89de9d34be50a3c5849c2ce75f.php
+++ storage/framework/views/c53088a374615e89de9d34be50a3c5849c2ce75f.php
@@ -23,44 +23,39 @@
             }
         </style>
         
-        <script>
-            let map;
-            let markers = [];
-
+    
+        <script type="text/javascript">
+            var locations = [
+                ['Bondi Beach', -33.890542, 151.274856, 4],
+                ['Coogee Beach', -33.923036, 151.259052, 5],
+                ['Cronulla Beach', -34.028249, 151.157507, 3],
+                ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+                ['Maroubra Beach', -33.950198, 151.259302, 1]
+            ];
             function initMap() {
-                map = new google.maps.Map(document.getElementById("map"), {
-                    zoom: 14,
-                    center: { lat: 53.893, lng:  27.567 },
-                    mapTypeId: "terrain",
-                });
-                // This event listener will call addMarker() when the map is clicked.
-                map.addListener("click", (event) => {
-                    addMarker(event.latLng);
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 10,
+                    center: new google.maps.LatLng(-33.92, 151.25),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
                 });
 
-            }
+                var infowindow = new google.maps.InfoWindow();
 
-            // Adds a marker to the map and push to the array.
-            function addMarker(location) {
-                const marker = new google.maps.Marker({
-                    position: location,
-                    map: map,
-                });
-                clearMarkers();
-                markers.push(marker);
-                document.getElementById('location').value = location.lat()+" , "+location.lng();
-            }
+                var marker, i;
 
-            // Sets the map on all markers in the array.
-            function setMapOnAll(map) {
-                for (let i = 0; i < markers.length; i++) {
-                    markers[i].setMap(map);
+                for (i = 0; i < locations.length; i++) {
+                    marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                        map: map
+                    });
+
+                    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                        return function() {
+                            infowindow.setContent(locations[i][0]);
+                            infowindow.open(map, marker);
+                        }
+                    })(marker, i));
                 }
-            }
-
-            // Removes the markers from the map, but keeps them in the array.
-            function clearMarkers() {
-                setMapOnAll(null);
             }
 
         </script>
