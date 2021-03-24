e4b957c166a0eeee545f6466c08e25fa9feaaf87
pasha <printcodestudio@gmail.com>
2021-03-24 16:21:22 +0300

    project EH

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,196 +14,146 @@
 
                 {{--Регистрация--}}
                 <div class="container">
-                    {{--Форма регистрации--}}
-               {{--     <form class="d-flex ">--}}
-{{--                        <input class="form-control mr-xl-1 cuprum"
-                               name="TextSearch"
-                               id="TextSearch"
-                               type="search"
-                               placeholder="ПОИСК"
-                               aria-label="Search">--}}
-                        @csrf
-                      {{--  <button class="btn btn-outline-success margin cuprum"
-                                type="submit"
-                                name="search"
-                                id="search">
-                            ПОИСК
-                        </button>--}}
+                   <form class="d-flex">
+                       <div class="cuprum ya-site-form ya-site-form_inited_no"
+                            data-bem="{&quot;action&quot;:&quot;https://yandex.ru/search/site/&quot;,&quot;arrow&quot;:false,&quot;bg&quot;:&quot;transparent&quot;,&quot;fontsize&quot;:16,&quot;fg&quot;:&quot;#000000&quot;,&quot;language&quot;:&quot;ru&quot;,&quot;logo&quot;:&quot;rb&quot;,&quot;publicname&quot;:&quot;Yandex Site Search #2454816&quot;,&quot;suggest&quot;:true,&quot;target&quot;:&quot;_blank&quot;,&quot;tld&quot;:&quot;ru&quot;,&quot;type&quot;:3,&quot;usebigdictionary&quot;:true,&quot;searchid&quot;:2454816,&quot;input_fg&quot;:&quot;#000000&quot;,&quot;input_bg&quot;:&quot;#ffffff&quot;,&quot;input_fontStyle&quot;:&quot;normal&quot;,&quot;input_fontWeight&quot;:&quot;normal&quot;,&quot;input_placeholder&quot;:&quot;Поиск по сайту&quot;,&quot;input_placeholderColor&quot;:&quot;#000000&quot;,&quot;input_borderColor&quot;:&quot;#000000&quot;}">
+                           <form class="d-flex"
+                                 action="https://yandex.ru/search/site/"
+                                 method="get"
+                                 target="_blank"
+                                 accept-charset="utf-8">
+                               <input type="hidden" name="searchid" value="2454816"/>
+                               <input type="hidden" name="l10n" value="ru"/>
+                               <input type="hidden" name="reqenc" value=""/>
+                               <input type="search" name="text" value=""/>
+                               <input class="" type="submit" value="Найти"/>
 
-                    {{--    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>ingredients</th>
-                            <th>id</th>
-                        </tr>
-                        </thead>
-                        <tbody></tbody>--}}
-
-                     {{--   <script>
-                            $(document).ready(function ()
-                            {
-                                function load_data(full_text_search_query = '')
-                                {
-                                    var _token = $("input[name=_token]").val();
-                                    $.ajax({
-                                        url:"{{route('TextSearch.action')}}",
-                                        method:"POST",
-                                        data:{full_text_search_query:full_text_search_query, _token:_token},
-                                        dataType:"json",
-                                        success:function (data)
-                                        {
-                                            var output = '';
-                                            if(data.length > 0)
-                                            {
-                                                for(var count = 0; count < data.length; count++)
-                                                {
-                                                    output += '<tr>';
-                                                    output += '<td>'+data[count].Name+'</td>';
-                                                    output += '<td>'+data[count].ingredients+'</td>';
-                                                    output += '<td>'+data[count].id+'</td>';
-                                                    output += '</tr>';
-                                                }
-                                            }
-                                            else
-                                            {
-                                                output += '<tr>';
-                                                output += '<td colspan="6">No Data Found</td>';
-                                                output += '</tr>';
-                                            }
-                                            $('tbody').html(output);
-                                        }
-
-
-                                    });
-                                }
-                                $('#search').click(function(){
-                                    var full_text_search_query = $('#full_text_search').val();
-                                    load_data(full_text_search_query);
-                                });
-                            });
-                        </script>--}}
-
-
-                        @if (Route::has('login'))
-
-                            @auth
-
-                            @else
-                                <a href="{{ route('login') }}" button class="btn btn-success margin cuprum">
-                                    ВХОД
-                                </a>
-
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" button class="btn btn-success margin cuprum">
-                                        РЕГИСТРАЦИЯ
-                                    </a>
-                                @endif
-
-                            @endauth
-
-                        @endif
-                        {{--Форма регистрации--}}
-
+                           </form>
+                       </div>
+                       <style
+                           type="text/css">.ya-page_js_yes .ya-site-form_inited_no { display: none; }
+                       </style>
+                       <script
+                           type="text/javascript">(function(w,d,c){var s=d.createElement('script'),h=d.getElementsByTagName('script')[0],e=d.documentElement;if((' '+e.className+' ').indexOf(' ya-page_js_yes ')===-1){e.className+=' ya-page_js_yes';}s.type='text/javascript';s.async=true;s.charset='utf-8';s.src=(d.location.protocol==='https:'?'https:':'http:')+'//site.yandex.net/v2.0/js/all.js';h.parentNode.insertBefore(s,h);(w[c]||(w[c]=[])).push(function(){Ya.Site.Form.init()})})(window,document,'yandex_site_callbacks');
+                       </script>
                     </form>
                 {{--Форма--}}
-
-                {{--Форма регистрации на бустрап--}}
-                    {{--<div class="modal fade" id="registration" tapindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="ModalLabelRegistration"></h5>
-                                    <button class="btn-close" data-dismiss="modal" aria-label="close"></button>
-                                </div>
-                                --}}{{--Тело регистрации--}}{{--
-                                <div class="modal-body">
-                                    <form class="border mt-5 mb-5">
-                                        <div class="form-group m-4">
-                                            <p class="text-center lead mt-5 mb-5 cuprum">
-                                                <b>РЕГИСТРАЦИЯ</b>
-                                            </p>
-                                            <input type="text" class="form-control cuprum" name="login" placeholder="Логин">
-                                        </div>
-                                        <div class="form-group m-4">
-                                            <input type="email" class="form-control cuprum" id="Email" aria-describedby="emailHelp" placeholder="Электронная почта">
-                                        </div>
-                                        <div class="form-group m-4">
-                                            <input type="password" class="form-control cuprum" id="Password" placeholder="Пароль">
-                                        </div>
-                                        <div class="form-check m-4">
-                                            <input type="checkbox" class="form-check-input" id="Check">
-                                            <label class="form-check-label" for="Check">Получить навосную рассылку</label>
-                                        </div>
-                                        <div class="text-center mb-5">
-                                            <button  type="submit" class="btn btn-success cuprum">ЗАРЕГИСТРИРОВАТЬСЯ</button>
-                                        </div>
-                                        <div>
-                                            <p class="text-center">
-                                                Нажимая на кнопку «Зарегистрироваться», вы подтверждаете свое согласия с условиями <a class="text-success text-decoration-none" href="">пользовательского соглашения.</a>
-
-                                                Уже зарегистрированы? <a class="text-success text-decoration-none" href=""> Войдите</a>
-                                            </p>
-                                        </div>
-                                        --}}{{--Тело регистрации--}}{{--
-                                    </form>
-                                </div>
-                                <div class="modal-footer">
-                                    <button class="btn btn-success cuprum" data-dismiss="modal">ВЫХОД</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>--}}
-                {{--Форма регистрации на бустрап--}}
-
-                {{--Форма регистрации на бустрап--}}
-                    {{--<div class="modal fade" id="authorization" tapindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="ModalLabelAuthorization"></h5>
-                                    <button class="btn-close" data-dismiss="modal" aria-label="close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <form class="border mt-5 mb-5">
-                                        <div class="form-group m-4">
-                                            <p class="text-center lead mt-5 mb-5 cuprum">
-                                                <b>ВХОД</b>
-                                            </p>
-                                        </div>
-                                        <div class="form-group m-4">
-                                            <input type="email" class="form-control cuprum" id="Email" aria-describedby="emailHelp" placeholder="Электронная почта">
-                                        </div>
-                                        <div class="form-group m-4">
-                                            <input type="password" class="form-control cuprum" id="Password" placeholder="Пароль">
-                                        </div>
-                                        <div class="form-check m-4">
-                                            <input type="checkbox" class="form-check-input" id="Check">
-                                            <label class="form-check-label" for="Check">Получить навосную рассылку</label>
-                                        </div>
-                                        <div class="text-center mb-5">
-                                            <button  type="submit" class="btn btn-success cuprum">ВОЙТИ</button>
-                                        </div>
-                                        <div>
-                                            <p class="text-center">
-                                                <a class="text-success text-decoration-none cuprum" href="">Забыли пароль?</a>
-
-                                                Ещё не с нами? <a class="text-success text-decoration-none cuprum" href="">Зарегистрируйтесь</a>
-                                            </p>
-                                        </div>
-
-                                    </form>
-                                </div>
-                                <div class="modal-footer">
-                                    <button class="btn btn-success cuprum" data-dismiss="modal">ВЫХОД</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>--}}
-                    {{--Форма регистрации на бустрап--}}
                 </div>
+                {{--Форма регистрации--}}
+                @if (Route::has('login'))
+
+                    @auth
+
+                    @else
+                        <a href="{{ route('login') }}" class="btn btn-success margin cuprum pt-3 pb-3">
+                            ВХОД
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="btn btn-success margin cuprum pt-3 pb-3">
+                                РЕГИСТРАЦИЯ
+                            </a>
+                        @endif
+
+                    @endauth
+
+                @endif
+                {{--Форма регистрации--}}
             </div>
         </nav>
     </header>
+{{--Форма регистрации на бустрап--}}
+{{--<div class="modal fade" id="registration" tapindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLabelRegistration"></h5>
+                <button class="btn-close" data-dismiss="modal" aria-label="close"></button>
+            </div>
+            --}}{{--Тело регистрации--}}{{--
+            <div class="modal-body">
+                <form class="border mt-5 mb-5">
+                    <div class="form-group m-4">
+                        <p class="text-center lead mt-5 mb-5 cuprum">
+                            <b>РЕГИСТРАЦИЯ</b>
+                        </p>
+                        <input type="text" class="form-control cuprum" name="login" placeholder="Логин">
+                    </div>
+                    <div class="form-group m-4">
+                        <input type="email" class="form-control cuprum" id="Email" aria-describedby="emailHelp" placeholder="Электронная почта">
+                    </div>
+                    <div class="form-group m-4">
+                        <input type="password" class="form-control cuprum" id="Password" placeholder="Пароль">
+                    </div>
+                    <div class="form-check m-4">
+                        <input type="checkbox" class="form-check-input" id="Check">
+                        <label class="form-check-label" for="Check">Получить навосную рассылку</label>
+                    </div>
+                    <div class="text-center mb-5">
+                        <button  type="submit" class="btn btn-success cuprum">ЗАРЕГИСТРИРОВАТЬСЯ</button>
+                    </div>
+                    <div>
+                        <p class="text-center">
+                            Нажимая на кнопку «Зарегистрироваться», вы подтверждаете свое согласия с условиями <a class="text-success text-decoration-none" href="">пользовательского соглашения.</a>
 
+                            Уже зарегистрированы? <a class="text-success text-decoration-none" href=""> Войдите</a>
+                        </p>
+                    </div>
+                    --}}{{--Тело регистрации--}}{{--
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-success cuprum" data-dismiss="modal">ВЫХОД</button>
+            </div>
+        </div>
+    </div>
+</div>--}}
+{{--Форма регистрации на бустрап--}}
+
+{{--Форма регистрации на бустрап--}}
+{{--<div class="modal fade" id="authorization" tapindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLabelAuthorization"></h5>
+                <button class="btn-close" data-dismiss="modal" aria-label="close"></button>
+            </div>
+            <div class="modal-body">
+                <form class="border mt-5 mb-5">
+                    <div class="form-group m-4">
+                        <p class="text-center lead mt-5 mb-5 cuprum">
+                            <b>ВХОД</b>
+                        </p>
+                    </div>
+                    <div class="form-group m-4">
+                        <input type="email" class="form-control cuprum" id="Email" aria-describedby="emailHelp" placeholder="Электронная почта">
+                    </div>
+                    <div class="form-group m-4">
+                        <input type="password" class="form-control cuprum" id="Password" placeholder="Пароль">
+                    </div>
+                    <div class="form-check m-4">
+                        <input type="checkbox" class="form-check-input" id="Check">
+                        <label class="form-check-label" for="Check">Получить навосную рассылку</label>
+                    </div>
+                    <div class="text-center mb-5">
+                        <button  type="submit" class="btn btn-success cuprum">ВОЙТИ</button>
+                    </div>
+                    <div>
+                        <p class="text-center">
+                            <a class="text-success text-decoration-none cuprum" href="">Забыли пароль?</a>
+
+                            Ещё не с нами? <a class="text-success text-decoration-none cuprum" href="">Зарегистрируйтесь</a>
+                        </p>
+                    </div>
+
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-success cuprum" data-dismiss="modal">ВЫХОД</button>
+            </div>
+        </div>
+    </div>
+</div>--}}
+{{--Форма регистрации на бустрап--}}
 @show
 
--- storage/framework/views/6b002127bd648fd17e0fcb56abc5d2486cde78b2.php
+++ storage/framework/views/6b002127bd648fd17e0fcb56abc5d2486cde78b2.php
@@ -2,6 +2,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+<meta name="Description" content="Рецепты,пошаговые рецепты,рецепты с фото,простые рецепты,вкусные рецепты,интересные рецепты,необычные рецепты,салаты,фото рецепт,суп,суши."/>
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
@@ -12,8 +13,29 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 <link rel="stylesheet" type="text/css" href="<?php echo e(mix('css/app.css')); ?>">
+<link rel="icon" href="https://example.com/favicon.ico" type="image/x-icon">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-<title>eh</title>
+    ym(74501833, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript>
+    <div>
+        <img src="https://mc.yandex.ru/watch/74501833" style="position:absolute; left:-9999px;" alt="" />
+    </div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
+
+<title>
+    edahelp.ru
+</title>
 
 <?php echo $__env->yieldSection(); ?>
 <?php /**PATH W:\domains\eh\resources\views/layouts/head.blade.php ENDPATH**/ ?>
