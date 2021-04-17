6524e6ca0964a14b8932327946f6e81e7c34418f
pasha <printcodestudio@gmail.com>
2021-04-17 18:25:48 +0300

    project EH

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,7 +1,7 @@
 @extends('home.home')
 @section('content')
     <main class="container-sm">
-        <div class="col-7 block-container border-right border-left border-dark">
+        <div class="col-8 block-container border-right border-left border-dark">
 
 {{-- ИНГРЕДИЕНТЫ --}}
 <main class="row">
@@ -16,22 +16,22 @@
         </article>
         {{-- ФОТО--}}
 
-        <ul class="list-group text-left h5 mt-2 ">
+        <ul class="list-group text-left h6 mt-2 ">
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class="cuprum mb-3">
                     {{ $article->articles_text_1 }}
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     {{ $article->articles_text_2 }}
                 </p>
             </li>
 
             <li>
                 {{-- ФОТО --}}
-                    <img class="img-fluid"
+                    <img class="img-fluid mb-3"
                          alt="Responsive image"
                          width="100%"
                          src="{{ asset($article->articles_image_3) }}">
@@ -39,49 +39,71 @@
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     {{ $article->articles_text_3 }}
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     {{ $article->articles_text_4 }}
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
-                    {{ $article->articles_text_5 }} <a href="https://clickmyt.com/BpZs">
-                        «ЗДОРОВ».
+                    <p class=" cuprum mb-3">
+                    {{ $article->articles_text_5 }} <a class="text-primary" href="{{ $article->product }}">
+                        {{ $article->name_product}}
                     </a>
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     {{ $article->articles_text_6 }}
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
-                    <a href="https://clickmyt.com/BpZs">
-                        «ЗДОРОВ»
+                <p class="cuprum mb-3">
+                    <a class="text-primary" href="{{ $article->product }}">
+                        {{ $article->name_product }}
                     </a>{{ $article->articles_text_7 }}
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-5">
+                <p class="cuprum mb-1">
                     {{ $article->articles_text_8 }}
                 </p>
             </li>
         </ul>
+
+        {{-- ЛЕНДИНГ --}}
+        <div class="row">
+            <div class="col-13 ">
+                {{-- ФОТО --}}
+                <a href="{{ $article->product }}">
+                <article class="">
+                    <img class="img-fluid"
+                         alt="Responsive image"
+                         width="100%"
+                         src="{{ asset($article->articles_image_4) }}">
+                </article>
+                {{-- ФОТО--}}
+                </a>
+                <div>
+                    <p CLASS="cuprum ">
+                        <b>
+                        </b>
+                    </p>
+                </div>
+            </div>
+        </div>
+        {{-- ЛЕНДИНГ --}}
+
     </div>
-    <a class="btn btn-success block-container" href="https://clickmyt.com/BpZs">
-        ЗАКАЗАТЬ
-    </a>
+
             </main>
         </div>
 </main>
--- storage/framework/views/ebf1cb87c5d57eb1cdfb6795d98e1da4c54ac57d.php
+++ storage/framework/views/ebf1cb87c5d57eb1cdfb6795d98e1da4c54ac57d.php
@@ -1,6 +1,6 @@
 <?php $__env->startSection('content'); ?>
     <main class="container-sm">
-        <div class="col-7 block-container border-right border-left border-dark">
+        <div class="col-8 block-container border-right border-left border-dark">
 
 
 <main class="row">
@@ -15,16 +15,16 @@
         </article>
         
 
-        <ul class="list-group text-left h5 mt-2 ">
+        <ul class="list-group text-left h6 mt-2 ">
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class="cuprum mb-3">
                     <?php echo e($article->articles_text_1); ?>
 
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     <?php echo e($article->articles_text_2); ?>
 
                 </p>
@@ -32,7 +32,7 @@
 
             <li>
                 
-                    <img class="img-fluid"
+                    <img class="img-fluid mb-3"
                          alt="Responsive image"
                          width="100%"
                          src="<?php echo e(asset($article->articles_image_3)); ?>">
@@ -40,54 +40,78 @@
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     <?php echo e($article->articles_text_3); ?>
 
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     <?php echo e($article->articles_text_4); ?>
 
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
-                    <?php echo e($article->articles_text_5); ?> <a href="https://clickmyt.com/BpZs">
-                        «ЗДОРОВ».
+                    <p class=" cuprum mb-3">
+                    <?php echo e($article->articles_text_5); ?> <a class="text-primary" href="<?php echo e($article->product); ?>">
+                        <?php echo e($article->name_product); ?>
+
                     </a>
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
+                <p class=" cuprum mb-3">
                     <?php echo e($article->articles_text_6); ?>
 
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-3">
-                    <a href="https://clickmyt.com/BpZs">
-                        «ЗДОРОВ»
+                <p class="cuprum mb-3">
+                    <a class="text-primary" href="<?php echo e($article->product); ?>">
+                        <?php echo e($article->name_product); ?>
+
                     </a><?php echo e($article->articles_text_7); ?>
 
                 </p>
             </li>
 
             <li>
-                <p class="font-italic cuprum mb-5">
+                <p class="cuprum mb-1">
                     <?php echo e($article->articles_text_8); ?>
 
                 </p>
             </li>
         </ul>
+
+        
+        <div class="row">
+            <div class="col-13 ">
+                
+                <a href="<?php echo e($article->product); ?>">
+                <article class="">
+                    <img class="img-fluid"
+                         alt="Responsive image"
+                         width="100%"
+                         src="<?php echo e(asset($article->articles_image_4)); ?>">
+                </article>
+                
+                </a>
+                <div>
+                    <p CLASS="cuprum ">
+                        <b>
+                        </b>
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+
     </div>
-    <a class="btn btn-success block-container" href="https://clickmyt.com/BpZs">
-        ЗАКАЗАТЬ
-    </a>
+
             </main>
         </div>
 </main>
